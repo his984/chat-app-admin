4b4790c38b4945f0c035ccc804c13762d93a925e
his984 <his984@gmail.com>
2023-01-20 21:10:59 +0200

    update database

--- src/backend/database/models/chatuser.js
+++ src/backend/database/models/chatuser.js
@@ -3,26 +3,23 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Session extends Model {
+    class ChatUser extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Session.belongsTo(models.User, {
-                foreignKey: 'userId',
-            });
+            ChatUser.belongsTo(models.Chat);
+            ChatUser.belongsTo(models.User);
         }
     }
 
-    Session.init({
-        token: DataTypes.TEXT,
-        agentInfo: DataTypes.JSON,
-
+    ChatUser.init({
+        status: DataTypes.ENUM
     }, {
         sequelize,
-        modelName: 'Session',
+        modelName: 'ChatUser',
     });
-    return Session;
+    return ChatUser;
 };
